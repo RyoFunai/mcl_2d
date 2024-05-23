15105fce78b4fcdad5688c521a7e100868a576e0
RyoFunai <s24s1040du@s.chibakoudai.jp>
2024-05-23 14:46:50 +0900

    map読み込み

--- mcl_2d/include/mcl_2d/mcl_2d_node.hpp
+++ mcl_2d/include/mcl_2d/mcl_2d_node.hpp
@@ -1,41 +1,58 @@
 #pragma once
-#include <rclcpp/rclcpp.hpp>
-#include <nav_msgs/msg/odometry.hpp>
-#include <sensor_msgs/msg/laser_scan.hpp>
-#include <geometry_msgs/msg/point.hpp>
-#include <geometry_msgs/msg/vector3.hpp>
-#include <geometry_msgs/msg/pose_array.hpp>
-
-
-#include <tf2/LinearMath/Matrix3x3.h>
-#include <tf2/LinearMath/Quaternion.h>
-#include <tf2_ros/static_transform_broadcaster.h>
+#include <tf2/LinearMath/Transform.h>
+#include <tf2/convert.h>
+#include <tf2/time.h>
+#include <tf2/utils.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/create_timer_ros.h>
+#include <tf2_ros/message_filter.h>
 #include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+
+#include <geometry_msgs/msg/point.hpp>
+#include <geometry_msgs/msg/pose_array.hpp>
+#include <geometry_msgs/msg/vector3.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 #include "mcl_2d/msg_converter.hpp"
 #include "mcl_2d/util.hpp"
 
-
 class Mcl2dNode : public rclcpp::Node {
-public:
+ public:
   Mcl2dNode();
   virtual ~Mcl2dNode() = default;
+  int odom_freq_;
 
-private:
+ private:
+  void loadMap(const std::string& yaml_path);
   void laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
-  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
-  void broadcastTransforms();
+  void initTF();
+  bool getOdomPose(double& x, double& y, double& yaw);
+  bool getLidarPose(double& x, double& y, double& yaw);
+  void publishOdomFrame(double x, double y, double t);
+  void loop();
 
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_subscription;
-  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr     odom_subscription;
 
-  std::shared_ptr<tf2_ros::TransformBroadcaster>              broadcaster;
-  rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr   selfpose_publisher;
+  std::shared_ptr<tf2_ros::TransformBroadcaster> broadcaster;
+  rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr selfpose_publisher;
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pc2_mapped_publisher;
   rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr particle_publisher;
 
   rclcpp::QoS _qos = rclcpp::QoS(40).keep_all();
+
+  std::shared_ptr<tf2_ros::Buffer> tf_;
+  std::shared_ptr<tf2_ros::TransformListener> tfl_;
+  std::shared_ptr<tf2_ros::TransformBroadcaster> tfb_;
+  tf2::Transform latest_tf_;
+  rclcpp::Clock ros_clock_;
+  std::string global_frame_id_, footprint_frame_id_, odom_frame_id_, base_frame_id_, scan_frame_id_;
+  rclcpp::Time scan_time_stamp_;
+  double transform_tolerance_;
 
   Vector3d odom = Vector3d::Zero();
   Vector3d laser = Vector3d::Zero();
@@ -43,4 +60,3 @@ private:
   MsgConverter msg_converter;
   Util util;
 };
-
--- mcl_2d/src/mcl_2d_node.cpp
+++ mcl_2d/src/mcl_2d_node.cpp
@@ -1,53 +1,148 @@
-#include <cstdio>
 #include "mcl_2d/mcl_2d_node.hpp"
 
-Mcl2dNode::Mcl2dNode() : Node("mcl_2d") {
+#include <cstdio>
+#include <yaml-cpp/yaml.h>
+#include <filesystem>
+
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+Mcl2dNode::Mcl2dNode() : Node("mcl_2d"),
+                         ros_clock_(RCL_SYSTEM_TIME) {
   laser_subscription = this->create_subscription<sensor_msgs::msg::LaserScan>(
       "scan", 10, std::bind(&Mcl2dNode::laser_callback, this, std::placeholders::_1));
-  odom_subscription = this->create_subscription<nav_msgs::msg::Odometry>(
-      "odom", 10, std::bind(&Mcl2dNode::odom_callback, this, std::placeholders::_1));
 
-  selfpose_publisher   = this->create_publisher<geometry_msgs::msg::Vector3>("self_pose", _qos);
+  selfpose_publisher = this->create_publisher<geometry_msgs::msg::Vector3>("self_pose", _qos);
   pc2_mapped_publisher = this->create_publisher<sensor_msgs::msg::PointCloud2>("pc2_mapped", _qos);
-  particle_publisher   = this->create_publisher<geometry_msgs::msg::PoseArray>("particlecloud", _qos);
-  broadcaster         = std::make_shared<tf2_ros::TransformBroadcaster>(*this);
+  particle_publisher = this->create_publisher<geometry_msgs::msg::PoseArray>("particlecloud", _qos);
+  broadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(*this);
 
-  this->declare_parameter<string>("param_name", "aaaaaa");
-  std::string param_value = this->get_parameter("param_name").as_string();
-  RCLCPP_INFO(this->get_logger(), "param_value : %s", param_value.c_str());
+	this->declare_parameter("global_frame_id", std::string("map"));
+	this->declare_parameter("footprint_frame_id", std::string("base_footprint"));
+	this->declare_parameter("odom_frame_id", std::string("odom"));
+	this->declare_parameter("base_frame_id", std::string("base_link"));
+	this->get_parameter("global_frame_id", global_frame_id_);
+	this->get_parameter("footprint_frame_id", footprint_frame_id_);
+	this->get_parameter("odom_frame_id", odom_frame_id_);
+	this->get_parameter("base_frame_id", base_frame_id_);
 
-  this->declare_parameter<int>("interval_time", 0);
-  int interval_time = this->get_parameter("interval_time").as_int();
-  RCLCPP_INFO(this->get_logger(), "interval : %d", interval_time);
+	this->declare_parameter("odom_freq", 20);
+	this->get_parameter("odom_freq", odom_freq_);
+
+  loadMap("iscas_museum_map.yaml");
+
+  initTF();
 }
 
-void Mcl2dNode::laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
+void Mcl2dNode::loop() {
+  double x, y, yaw;
+  if (getOdomPose(x, y, yaw) && getLidarPose(x, y, yaw)) {
+    RCLCPP_INFO(this->get_logger(), "x: %f, y: %f, yaw: %f", x, y, yaw);
+  }
+}
+
+void Mcl2dNode::loadMap(const std::string& yaml_path) {
+  std::filesystem::path source_dir = std::filesystem::path(__FILE__).parent_path();
+  std::filesystem::path map_dir = source_dir / "../../config/maps";
+  YAML::Node config = YAML::LoadFile(map_dir.string() + "/" + yaml_path);
+  std::filesystem::path image_path = map_dir / config["image"].as<std::string>();
+  double resolution = config["resolution"].as<double>();
+  std::vector<double> origin = config["origin"].as<std::vector<double>>();
+  int negate = config["negate"].as<int>();
+  double occupied_thresh = config["occupied_thresh"].as<double>();
+  double free_thresh = config["free_thresh"].as<double>();
+
+  cv::Mat map_image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
+  if (map_image.empty()) {
+    RCLCPP_ERROR(this->get_logger(), "Failed to load map image: %s", image_path.c_str());
+    return;
+  }
+  
+  cv::imshow("Map", map_image);
+  cv::waitKey(0);  // キー入力を待つ
+
+  RCLCPP_INFO(this->get_logger(), "Map loaded successfully: %s", image_path.c_str());
+}
+
+
+void Mcl2dNode::initTF() {
+  tfb_.reset();
+  tfl_.reset();
+  tf_.reset();
+
+  tf_ = std::make_shared<tf2_ros::Buffer>(get_clock());
+  auto timer_interface = std::make_shared<tf2_ros::CreateTimerROS>(
+      get_node_base_interface(), get_node_timers_interface(),
+      create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive, false));
+  tf_->setCreateTimerInterface(timer_interface);
+  tfl_ = std::make_shared<tf2_ros::TransformListener>(*tf_);
+  tfb_ = std::make_shared<tf2_ros::TransformBroadcaster>(shared_from_this());
+  latest_tf_ = tf2::Transform::getIdentity();
+}
+
+void Mcl2dNode::laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
   RCLCPP_INFO(this->get_logger(), "Received scan");
-  vector<LaserPoint> src_points = msg_converter.scan_to_vector(msg, odom);  //本来laserの位置
+
+	scan_frame_id_ = msg->header.frame_id;
+  vector<LaserPoint> src_points = msg_converter.scan_to_vector(msg, odom);  // 本来laserの位置
   sensor_msgs::msg::PointCloud2 cloud = msg_converter.vector_to_PC2(src_points);
   pc2_mapped_publisher->publish(cloud);
 }
 
-void Mcl2dNode::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) {
-  RCLCPP_INFO(this->get_logger(), "Received odom: position x: %f, y: %f", msg->pose.pose.position.x, msg->pose.pose.position.y);
-  odom[0] = msg->pose.pose.position.x;
-  odom[1] = msg->pose.pose.position.y;
-  odom[2] = util.quaternionToYaw(msg->pose.pose.orientation.w, msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z);
-  broadcastTransforms();
+bool Mcl2dNode::getOdomPose(double& x, double& y, double& yaw) {
+  geometry_msgs::msg::PoseStamped ident;
+  ident.header.frame_id = footprint_frame_id_;
+  ident.header.stamp = rclcpp::Time(0);
+  tf2::toMsg(tf2::Transform::getIdentity(), ident.pose);
+
+  geometry_msgs::msg::PoseStamped odom_pose;
+  try {
+    this->tf_->transform(ident, odom_pose, odom_frame_id_);
+  } catch (tf2::TransformException& e) {
+    RCLCPP_WARN(get_logger(), "Failed to compute odom pose, skipping scan (%s)", e.what());
+    return false;
+  }
+  x = odom_pose.pose.position.x;
+  y = odom_pose.pose.position.y;
+  yaw = tf2::getYaw(odom_pose.pose.orientation);
+
+  return true;
 }
 
-void Mcl2dNode::broadcastTransforms() {
-  std::vector<double> tf_laser2robot = {1.0, 2.0, 0.0, 0.0, 0.0, 0.0};
-  geometry_msgs::msg::TransformStamped world_to_base_link = msg_converter.broadcastWorldToBaseLink(odom[0], odom[1], odom[2]);
-  geometry_msgs::msg::TransformStamped base_link_to_lidar = msg_converter.broadcastBaseLinkToLidarFrame(tf_laser2robot);
-  broadcaster->sendTransform(world_to_base_link);
-  broadcaster->sendTransform(base_link_to_lidar);
+bool Mcl2dNode::getLidarPose(double& x, double& y, double& yaw) {
+  geometry_msgs::msg::PoseStamped ident;
+  ident.header.frame_id = scan_frame_id_;
+  ident.header.stamp = ros_clock_.now();
+  tf2::toMsg(tf2::Transform::getIdentity(), ident.pose);
+
+  geometry_msgs::msg::PoseStamped lidar_pose;
+  try {
+    this->tf_->transform(ident, lidar_pose, base_frame_id_);
+  } catch (tf2::TransformException& e) {
+    RCLCPP_WARN(get_logger(), "Failed to compute lidar pose, skipping scan (%s)", e.what());
+    return false;
+  }
+
+  x = lidar_pose.pose.position.x;
+  y = lidar_pose.pose.position.y;
+
+  double roll, pitch;
+  tf2::getEulerYPR(lidar_pose.pose.orientation, yaw, pitch, roll);
+
+  return true;
 }
 
-int main(int argc, char ** argv){
+int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
 
   auto node = std::make_shared<Mcl2dNode>();
+	rclcpp::Rate loop_rate(node->odom_freq_);
+	while (rclcpp::ok()) {
+		// node->loop();
+		rclcpp::spin_some(node);
+		loop_rate.sleep();
+	}
 
   rclcpp::spin(node);
 
