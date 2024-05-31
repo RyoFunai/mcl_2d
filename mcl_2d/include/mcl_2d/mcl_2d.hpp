e40a2103b0f28a4763ca8a4b20375ad7d6f605d6
RyoFunai <s24s1040du@s.chibakoudai.jp>
2024-05-31 19:42:06 +0900

    マップと画像間の変換を確認

--- mcl_2d/include/mcl_2d/mcl_2d.hpp
+++ mcl_2d/include/mcl_2d/mcl_2d.hpp
@@ -1,33 +1,39 @@
 #pragma once
 
+#include <cmath>
 #include <eigen3/Eigen/Dense>
 #include <opencv2/opencv.hpp>
 #include <random>
-#include <cmath>
 #include <rclcpp/rclcpp.hpp>
+
+#include "scan.hpp"
 
 using namespace Eigen;
 using namespace std;
 
-class Mcl2d
-{
-public:
-  struct Particle{
+class Mcl2d {
+ public:
+  struct Particle {
     Eigen::Matrix4f pose;
     float score;
   };
 
   void loadMap(const string& yaml_path);
-  void init_particles(const Vector3d& initial_pose, const int particles_num);
+  void generate_particles(const Vector3f& initial_pose, const int particles_num);
   vector<Particle> getParticles() { return particles; };
+  Vector3f updateData(const Vector3f& pose, const vector<LaserPoint>& src_points, const int particles_num);
 
+ private:
+  void likelihood(const vector<LaserPoint>& src_points);
+  Vector3f estimate_current_pose();
+  pair<int, int> mapTopixel(const float& x, const float& y);
+  pair<float, float> pixelToMap(const int& x, const int& y);
+  void displayPointOnMap(const float& x, const float& y);
 
-
-
-private:
-  cv::Mat gridMap;
+  cv::Mat map_image;
   rclcpp::Node::SharedPtr node_;
   vector<Particle> particles;
   double image_resolution;
-  Vector3d origin_;
+  Vector3f map_origin;
+  Particle highest_weight_particle;
 };
--- mcl_2d/src/mcl_2d.cpp
+++ mcl_2d/src/mcl_2d.cpp
@@ -13,37 +13,38 @@ void Mcl2d::loadMap(const string& yaml_path) {
   filesystem::path image_path = yaml_file_path.parent_path() / config["image"].as<string>();
   image_resolution = config["resolution"].as<double>();
   vector<double> origin = config["origin"].as<vector<double>>();
-  origin_ << origin[0], origin[1], origin[2];
+  map_origin << origin[0], origin[1], origin[2];
   // int negate = config["negate"].as<int>();
   // double occupied_thresh = config["occupied_thresh"].as<double>();
   // double free_thresh = config["free_thresh"].as<double>();
 
-  cv::Mat map_image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
-  if (map_image.empty()) {
+  cv::Mat src_map_image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
+  if (src_map_image.empty()) {
     RCLCPP_ERROR(rclcpp::get_logger("mcl_2d"), "Failed to load map image: %s", image_path.c_str());
     return;
   }
 
   cv::Mat flipped_map_image;
-  cv::flip(map_image, flipped_map_image, 0);
+  cv::flip(src_map_image, flipped_map_image, 0);
   // imshow("Map", flipped_map_image);
   // cv::waitKey(0);
 
-  gridMap = flipped_map_image.clone();
+  map_image = flipped_map_image.clone();
+  displayPointOnMap(1.f, 2.f);
+
   RCLCPP_INFO(rclcpp::get_logger("mcl_2d"), "Map loaded successfully: %s", image_path.c_str());
 }
 
-void Mcl2d::init_particles(const Vector3d& initial_pose, const int particles_num) {
+void Mcl2d::generate_particles(const Vector3f& pose, const int particles_num) {
   particles.clear();
 
-  // normal_distribution<float> x_pos(initial_pose[0], gridMap.cols * image_resolution / 4.0);
-  // normal_distribution<float> y_pos(initial_pose[1], gridMap.rows * image_resolution / 4.0);
+  // normal_distribution<float> x_pos(initial_pose[0], map_image.cols * image_resolution / 4.0);
+  // normal_distribution<float> y_pos(initial_pose[1], map_image.rows * image_resolution / 4.0);
   // normal_distribution<float> theta_pos(initial_pose[2], M_PI / 4);
 
-  Vector3d map_initial_pose = initial_pose - origin_;
-  std::normal_distribution<float> x_pos(initial_pose[0], 0.1);  // Standard deviation of 1.0
-  std::normal_distribution<float> y_pos(initial_pose[1], 0.1);  // Standard deviation of 1.0
-  std::normal_distribution<float> theta_pos(initial_pose[2], M_PI / 8);
+  std::normal_distribution<float> x_pos(pose[0], 0.1);
+  std::normal_distribution<float> y_pos(pose[1], 0.1);
+  std::normal_distribution<float> theta_pos(pose[2], M_PI / 8);
 
   for (int i = 0; i < particles_num; i++) {
     Particle particle_temp;
@@ -57,40 +58,94 @@ void Mcl2d::init_particles(const Vector3d& initial_pose, const int particles_num
     particle_temp.score = 1 / (double)particles_num;
     particles.push_back(particle_temp);
 
-    RCLCPP_INFO(rclcpp::get_logger("mcl_2d"), "Particle %d: x=%f, y=%f, yaw=%f, score=%f",
-                i, particle_temp.pose(0, 3), particle_temp.pose(1, 3),
-                std::atan2(particle_temp.pose(1, 0), particle_temp.pose(0, 0)), particle_temp.score);
+    // RCLCPP_INFO(rclcpp::get_logger("mcl_2d"), "Particle %d: x=%f, y=%f, yaw=%f, score=%f",
+    //             i, particle_temp.pose(0, 3), particle_temp.pose(1, 3),
+    //             std::atan2(particle_temp.pose(1, 0), particle_temp.pose(0, 0)), particle_temp.score);
   }
 }
-// int Mcl2d::setup(const int particles_num, const float odomCovariance[6], const Matrix4f tf_laser2robot, const Matrix4f initial_pose) {
-//   //--YOU CAN CHANGE THIS PARAMETERS BY YOURSELF--//
-//   this->particles_num = particles_num;  // Number of Particles.
-//   minOdomDistance = 0.001;              // [m]
-//   minOdomAngle = 0.1;                   // [deg]
-//   repropagateCountNeeded = 1;           // [num]
 
-//   for (int i = 0; i < 6; i++) {
-//     this->odomCovariance[i] = odomCovariance[i];
-//   }
+Vector3f Mcl2d::updateData(const Vector3f& pose, const vector<LaserPoint>& src_points, const int particles_num) {
+  RCLCPP_INFO(rclcpp::get_logger("mcl_2d"), "Updating data...");
+  generate_particles(pose, particles_num);
+  likelihood(src_points);
+  return estimate_current_pose();
+}
 
-//   //--DO NOT TOUCH THIS PARAMETERS--//
-//   image_resolution = 0.05;  // [m] per [pixel]
+void Mcl2d::likelihood(const vector<LaserPoint>& src_points) {
+  float max_score = 0;
+  float score_sum = 0;
 
-//   this->tf_laser2robot = tf_laser2robot;
-//   this->initial_pose = initial_pose;
+  for (auto& particle : particles) {
+    float weight = 0;
 
-//   VectorXf initial_xyzrpy = tool::eigen2xyzrpy(initial_pose);
-//   x = initial_xyzrpy[0];
-//   y = initial_xyzrpy[1];
-//   angle = initial_xyzrpy[5];
+    for (auto& point : src_points) {
+      Eigen::Vector4f laser_point(point.x, point.y, 0, 1);
+      Eigen::Vector4f map_point = particle.pose * laser_point;
 
-//   mapCenterX = 0;             // [m]
-//   mapCenterY = 0;             // [m]
-//   isOdomInitialized = false;  // Will be true when first data incoming.
-//   predictionCounter = 0;
+      auto [image_pt_x, image_pt_y] = mapTopixel(map_point.x(), map_point.y());
 
-//   initializeParticles();  // Initialize particles.
-//   showInMap();
+      if (image_pt_x < 0 || image_pt_x >= map_image.cols || image_pt_y < 0 || image_pt_y >= map_image.rows)
+        continue;
+      else {
+        double pixel_val = map_image.at<uchar>(image_pt_y, image_pt_x) / (double)255;
+        weight += pixel_val;
+      }
+    }
+    particle.score = particle.score + (weight / src_points.size());
+    score_sum += particle.score;
+    if (max_score < particle.score) {
+      highest_weight_particle = particle;
+      // highest_weight_particle.scan = laser;
+      max_score = particle.score;
+    }
+  }
+  for (int i = 0; i < (int)particles.size(); i++) {
+    particles.at(i).score = particles.at(i).score / score_sum;  // 正規化
+  }
+}
 
-//   return 0;
-// }
+Vector3f Mcl2d::estimate_current_pose() {
+  Vector3f pose = Vector3f::Zero();
+
+  if (highest_weight_particle.score > 0) {
+    pose[0] = static_cast<float>(highest_weight_particle.pose(0, 3));
+    pose[1] = static_cast<float>(highest_weight_particle.pose(1, 3));
+    pose[2] = static_cast<float>(atan2f(highest_weight_particle.pose(1, 0), highest_weight_particle.pose(0, 0)));
+    RCLCPP_INFO(rclcpp::get_logger("mcl_2d"), "Highest Weight Particle  x: %f, y: %f, yaw: %f, score: %f", pose[0], pose[1], pose[2], highest_weight_particle.score);
+
+  } else {
+    RCLCPP_WARN(rclcpp::get_logger("mcl_2d"), "No particle with positive score");
+  }
+  return pose;
+}
+
+pair<int, int> Mcl2d::mapTopixel(const float& x, const float& y) {
+  int pixel_x = static_cast<int>((x - map_origin[0]) / image_resolution);
+  int pixel_y = static_cast<int>((y - map_origin[1]) / image_resolution);
+  return std::make_pair(pixel_x, pixel_y);
+}
+
+pair<float, float> Mcl2d::pixelToMap(const int& x, const int& y) {
+  float map_x = (x * image_resolution) + map_origin[0];
+  float map_y = (y * image_resolution) + map_origin[1];
+  return std::make_pair(map_x, map_y);
+}
+
+void Mcl2d::displayPointOnMap(const float& x, const float& y) {
+  auto [pixel_x, pixel_y] = mapTopixel(x, y);
+  auto [map_x, map_y] = pixelToMap(pixel_x, pixel_y);
+
+  cv::Mat color_map_image;
+  cv::cvtColor(map_image, color_map_image, cv::COLOR_GRAY2BGR);
+
+  std::string text = "(" + std::to_string(map_x) + ", " + std::to_string(map_y) + ")";
+  cv::putText(color_map_image, text, cv::Point(pixel_x, pixel_y), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 0, 0), 1);
+  cv::circle(color_map_image, cv::Point(pixel_x, pixel_y), 4, cv::Scalar(0, 255, 0), -1);
+
+  auto [o_x, o_y] = mapTopixel(0.f, 0.f);
+  cv::circle(color_map_image, cv::Point(o_x, o_y), 2, cv::Scalar(0, 0, 255), -1);
+
+  cv::imshow("Map Image", color_map_image);
+  cv::waitKey(0);  // キー入力を待つ
+  exit(0);
+}
